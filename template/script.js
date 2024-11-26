ea63d49b61aba7fd2b29f0229d84e4a9696ca224
CalterTV <jack511mendez@gmail.com>
2024-11-26 17:10:01 -0500

    Add files via upload

---
+++ template/script.js
@@ -0,0 +1,4 @@
+function showDiscordButton() {
+    // Muestra el botón de Discord solo cuando se hace clic en la imagen 3
+    document.getElementById('discord-button-container').style.display = 'block';
+}
